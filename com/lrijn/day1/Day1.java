4d88152785d8596315dd035d545cfa9c0cfa6cd0
Lisa van Rijn <lvanrijn@ilionx.com>
2024-09-16 14:45:24 +0200

    Finish day 1 part 1

--- com/lrijn/day1/Day1.java
+++ com/lrijn/day1/Day1.java
@@ -1,38 +1,43 @@
 package com.lrijn.day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Day1 {
     public static void main (String[] args){
-        String calories  = """
-                1000
-                2000
-                3000
-                
-                4000
-                
-                5000
-                6000
-                
-                7000
-                8000
-                9000
-                
-                10000
-                """;
 
-        int sumOfCal = 0;
-        int highestCal = 0;
-
-        for (String s : calories.split("\n")) {
-            String cal = s.trim();
-            if(!cal.isBlank()){
-                sumOfCal += Integer.parseInt(s);
-            }else{
-                if (sumOfCal > highestCal){
-                    highestCal = sumOfCal;
-                }
-                sumOfCal = 0;
+        try{
+            File myObj = new File("com/lrijn/day1/input.txt");
+            Scanner scanner = new Scanner(myObj);
+            StringBuilder fileContent = new StringBuilder();
+            while(scanner.hasNextLine()){
+                String data = scanner.nextLine();
+                fileContent.append(data).append("\n");
             }
+            scanner.close();
+
+            String calories  = fileContent.toString();
+
+            int sumOfCal = 0;
+            int highestCal = 0;
+
+            for (String s : calories.split("\n")) {
+                String cal = s.trim();
+                if(!cal.isBlank()){
+                    sumOfCal += Integer.parseInt(s);
+                }else{
+                    if (sumOfCal > highestCal){
+                        highestCal = sumOfCal;
+                    }
+                    sumOfCal = 0;
+                }
+            }
+            System.out.println("Highest number of calories: " + highestCal);
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
         }
-        System.out.println("Highest number of calories: " + highestCal);
+
     }
 }
